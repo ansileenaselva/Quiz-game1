3d566e1fa6b535263b9a6259d0b493250022338f
ansileenaselva <ansileenaselva@gmail.com>
2024-10-05 20:21:14 -0700

    add code for project

---
+++ proj 1.py
@@ -0,0 +1,30 @@
+print("welcome to our game")
+player=input("do you want to play")
+if player!="yes":
+    print("ok lets play")
+else:
+    print("not correct")
+
+question=input("how many days you have in a week")
+if question=="seven":
+    print("correct")
+else:
+    print("not correct")
+
+inputquestion=input("in which the direction of sun")
+if question=="east":
+    print("correct")
+else:
+    print("not correct")
+
+question=input("which river is big")
+if question=="ganga":
+    print("correct")
+else:
+    print("not correct")
+
+question=input("who is mother of gk")
+if question=="ganga":
+    print("correct")
+else:
+    print("not correct")
